d8a177a7dcfe30853dd3ba1a3e887ff00afcfd45
lizejiong <2571457002@qq.com>
2026-07-11 16:34:54 +0800

    feat: add Answer model and relationship to User and Exam models; update tsconfig for excel library path

--- apps/answer/src/answer.module.ts
+++ apps/answer/src/answer.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AnswerController } from './answer.controller';
 import { AnswerService } from './answer.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard, CommonModule } from '@app/common';
+import { PrismaModule } from '@app/prisma';
+import { ExcelModule } from '@app/excel';
 
 @Module({
   imports: [
+    PrismaModule,
+    CommonModule,
     ClientsModule.register([
       {
         name: 'EXAM_SERVICE',
@@ -14,8 +20,15 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         },
       },
     ]),
+    ExcelModule,
   ],
   controllers: [AnswerController],
-  providers: [AnswerService],
+  providers: [
+    AnswerService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AnswerModule {}
--- apps/answer/src/main.ts
+++ apps/answer/src/main.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AnswerModule } from './answer.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AnswerModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3003);
 }
 bootstrap();
